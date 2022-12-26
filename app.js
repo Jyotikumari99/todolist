c1929a6bf39a8423b4690037afa6f2752876c412
Jyotikumari99 <chunniyadav99@gmail.com>
2022-12-26 16:13:29 +0530

    added html and css javascript is adding

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input=document.querySelector('#new-task-input');
+  const list_li=document.querySelector('#tasks');
+//   console.log(form);
+form.addEventListener('submit',(e)=>{
+     e.preventDefault();
+     // console.log("submit fomr");
+     const task=input.ariaValueMax;
+     if(!text)
+     {
 
+          alert("Please add a task");
+     }
+     else
+     {
+          console.log("Success")
+     }
+})
+});
